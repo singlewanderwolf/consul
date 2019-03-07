consuldemocracy/consuldemocracy

Do not display polls associated to a budget in admin poll questions
This section is used to select to which poll a question belongs to.

Budget polls are not meant to include questions that come from Citizen
Proposals or Government Questions, thus we do not display them

spec/models/poll/poll_spec.rb
@@ -291,4 +291,24 @@ describe Poll do
 
   end
 
+  context "scopes" do
+
+    describe "#not_budget" do
+
+      it "returns polls not associated to a budget" do
+        budget = create(:budget)
+
+        poll1 = create(:poll)
+        poll2 = create(:poll)
+        poll3 = create(:poll, budget: budget)
+
+        expect(Poll.not_budget).to include(poll1)
+        expect(Poll.not_budget).to include(poll2)
+        expect(Poll.not_budget).to_not include(poll3)
+      end
+
+    end
+
+  end
+
 end

singlewanderwolf/consul

spec/models/poll/poll_spec.rb
@@ -261,4 +261,24 @@ describe Poll do
 
   end
 
+  context "scopes" do
+
+    describe "#not_budget" do
+
+      it "returns polls not associated to a budget" do
+        budget = create(:budget)
+
+        poll1 = create(:poll)
+        poll2 = create(:poll)
+        poll3 = create(:poll, budget: budget)
+
+        expect(Poll.not_budget).to include(poll1)
+        expect(Poll.not_budget).to include(poll2)
+        expect(Poll.not_budget).not_to include(poll3)
+      end
+
+    end
+
+  end
+
 end